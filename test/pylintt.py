6312f2a42c592f466261d8a325b30f96cf9279f2
RAHMOUNI <youssrarahmouni41@gmail.com>
2021-12-13 15:07:11 +0000

    Update pylintt.py

--- test/pylintt.py
+++ test/pylintt.py
@@ -2,6 +2,7 @@ import pylint.lint
 import pylint.reporters.text as text
 import io
 import os
+import sys
 pylint_opts = ['--disable=trailing-whitespace', sys.argv]
 pylint.lint.Run(pylint_opts)
 #os.stat("report.out").st_size == 0
