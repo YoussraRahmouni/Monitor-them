5f50251b9b363655c50ac34ecd0f5b4e4f532aca
Manal DERMICH <manal.dermich@gmail.com>
2021-12-13 15:25:08 +0000

    Update pylintt.py

--- test/pylintt.py
+++ test/pylintt.py
@@ -3,7 +3,7 @@ import pylint.reporters.text as text
 import io
 import os
 import sys
-for file in sys.argv:
+for file in sys.argv[1:]:
     print(file)
     pylint_opts = ['--disable=trailing-whitespace', file]
     pylint.lint.Run(pylint_opts)
