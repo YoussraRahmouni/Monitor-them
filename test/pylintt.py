90a3221f70a310b8db7ffe506927ee35c95c82ba
Manal DERMICH <manal.dermich@gmail.com>
2021-12-13 14:35:37 +0000

    Update pylintt.py

--- test/pylintt.py
+++ test/pylintt.py
@@ -2,9 +2,6 @@ import pylint.lint
 import pylint.reporters.text as text
 import io
 import os
-pylint_opts = ['--disable=trailing-whitespace', '../script1.py']
-pylint_output = io.StringIO() # Custom open stream
-with open("report.out", "w") as f:
-    reporter = text.TextReporter(f)
-    pylint.lint.Run(pylint_opts, reporter=reporter, do_exit=False)
-os.stat("report.out").st_size == 0
+pylint_opts = ['--disable=trailing-whitespace', 'connexion.py']
+pylint.lint.Run(pylint_opts)
+#os.stat("report.out").st_size == 0
