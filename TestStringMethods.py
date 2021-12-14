67313d5cb9d4e252414dec25357f2838987b9dcd
Maxime Perrin <maxime.perrin@telecom-st-etienne.fr>
2021-12-14 14:05:28 +0100

    add test_parser

--- TestStringMethods.py
+++ TestStringMethods.py
@@ -1,15 +1,14 @@
 import unittest
-from script1 import sshcmd
+from script1 import log_parsing
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_free(self):
-        self.assertTrue("total" in sshcmd("161.3.160.65", 22, "interfadm", "Projet654!", "free"))
-    def test_ps(self):
-        self.assertTrue("CMD" in sshcmd("161.3.160.65", 22, "interfadm", "Projet654!", "ps"))
-    def test_vmstat(self):
-        self.assertTrue("memory" in sshcmd("161.3.160.65", 22, "interfadm", "Projet654!", "vmstat"))
-
+    def test_parser(self):
+        template = '127.1.1.1 - - [09/Jan/2011:18:22:48 +0100] "GET / HTTP/1.1" 302 243 "-" "Mozilla/5.0 (X11; U; Linux i686; fr; rv:1.9.2.13) Gecko/20101206 Ubuntu/10.04 (lucid) Firefox/3.6.13"'
+        self.assertEqual(log_parsing(template)['remote_host'], '127.1.1.1')
+        self.assertEqual(log_parsing(template)['status'], '302')
+        self.assertEqual(log_parsing(template)['time_received'], '[09/Jan/2011:18:22:48 +0100]')
+        self.assertEqual(log_parsing(template)['request_method'], 'GET')
 
 if __name__ == '__main__':
     unittest.main()
--- script1.py
+++ script1.py
@@ -1,4 +1,5 @@
 import paramiko
+import apache_log_parser
 
 def sshcmd(hostname, port, username, password, command):
     client = paramiko.SSHClient()
@@ -17,13 +18,13 @@ def sshcmd(hostname, port, username, password, command):
         if client:
             client.close()
     return output
-    
+
 #sshcmd("161.3.160.65", 22, "interfadm", "Projet654!", "ifconfig -a")
 
 log_line=sshcmd("161.3.160.65", 22, "interfadm", "Projet654!","head /var/log/apache2/access.log")
+print(log_line)
 
 def log_parsing(log_line):
-    
     line_parser = apache_log_parser.make_parser("%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"")
-    log_line_data=line_parser(log_line_data)
+    log_line_data=line_parser(log_line)
     return(log_line_data)
