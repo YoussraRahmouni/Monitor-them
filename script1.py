cd0dfd5599b499840a7b3815abc99efa02c6eef6
LAURENCE <remi.laurence@hotmail.com>
2022-01-04 10:00:46 +0000

    Update script1.py

--- script1.py
+++ script1.py
@@ -2,7 +2,7 @@ import paramiko
 import apache_log_parser
 from pprint import pprint
 import datetime
-from datetime import tzinfo
+import time
 
 def sshcmd(hostname, port, username, password, command):
     client = paramiko.SSHClient()
@@ -15,16 +15,18 @@ def sshcmd(hostname, port, username, password, command):
     output = stdout.read().decode("utf-8")
     #for line in output.splitlines():
         #print(line)
+    client.close();
     return output
+
 
 
 #sshcmd("161.3.160.65", 22, "interfadm", "Projet654!", "tail -100 /var/log/apache2/access.log")
 
-#log=sshcmd("161.3.160.65", 22, "interfadm", "Projet654!","tail -1 /var/log/apache2/access.log")
+#log=sshcmd("37.110.193.25", 22, "interfadm", "Projet654!","tail -1 /var/log/apache2/access.log.1")
 
+MemUsed=sshcmd("37.110.193.25", 22, "interfadm", "Projet654!","free | tail -2 | head -1 | awk '{ print $5 }' ")
 
-
-
+CpuUsage=sshcmd("37.110.193.25", 22, "interfadm", "Projet654!","cat /proc/stat |grep cpu |tail -1|awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}'|awk '{print 100-$1}'")
 
 
 def log_parsing(log_line):
@@ -37,10 +39,13 @@ def log_parsing(log_line):
 ###
 #test_parser= '127.0.0.1 - - [07/Dec/2021:12:26:14 +0100] "GET /index.html HTTP/1.1" 200 3477 "-" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0"'
 
-previous_date=datetime.datetime(2021, 12, 7, 12, 26, 23)
+previous_date=datetime.datetime(2022, 1, 3, 19, 0, 0)
+
+
+line_parser = apache_log_parser.make_parser("%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"")
 
 def count_difference():
-    last_log=sshcmd("161.3.160.65", 22, "interfadm", "Projet654!","tail -1 /var/log/apache2/access.log")
+    last_log=sshcmd("monitorme1.ddns.net", 22, "interfadm", "Projet654!","tail -1 /var/log/apache2/access.log.1")
     parsed_last_log=log_parsing(last_log)
     last_line_date=parsed_last_log["time_received_datetimeobj"]
 
@@ -48,7 +53,7 @@ def count_difference():
     ip_list=[]
     error_count=0
 
-
+   
 
     if last_line_date >= previous_date :
         count_page_list=[]
@@ -57,8 +62,8 @@ def count_difference():
         page_list=[[parsed_last_log['request_first_line'],1]]
         while log_date > previous_date:
 
-            log="tail -"+str(n)+" /var/log/apache2/access.log | head -1"
-            current_log=sshcmd("161.3.160.65", 22, "interfadm", "Projet654!",log);
+            log="tail -"+str(n)+" /var/log/apache2/access.log.1 | head -1"
+            current_log=sshcmd("monitorme1.ddns.net", 22, "interfadm", "Projet654!",log);
             current_log_data=line_parser(current_log)#Dictionnary
             log_date=current_log_data["time_received_datetimeobj"]#timestamp
 
@@ -89,8 +94,18 @@ def count_difference():
 
 
             n+=1
-
+        
+        
+        
         print(page_list)
-        print(error_count)
-        print(len(ip_list))
+        print("Error Count : ",error_count)
+        print("Unique Ips : ",len(ip_list))
         print(n-1)
+
+
+ MemUsed=sshcmd("37.110.193.25", 22, "interfadm", "Projet654!","free | tail -2 | head -1 | awk '{ print $5 }' ")
+
+CpuUsage=sshcmd("37.110.193.25", 22, "interfadm", "Projet654!","cat /proc/stat |grep cpu |tail -1|awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}'|awk '{print 100-$1}'")
+
+print("Cpu Usage : ",CpuUsage)
+print("Memory Used : ",MemUsed)
