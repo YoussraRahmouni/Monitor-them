da436abef1784c2c0aa6b2dc450fd7114e8ade76
LAURENCE <remi.laurence@hotmail.com>
2022-01-07 10:37:25 +0000

    different functions for machines1 and machines2/3

--- script1.py
+++ script1.py
@@ -1,7 +1,7 @@
 import paramiko
 import apache_log_parser
 from pprint import pprint
-import datetime
+from datetime import datetime, timedelta
 import time
 
 def sshcmd(hostname, port, username, password, command):
@@ -15,7 +15,7 @@ def sshcmd(hostname, port, username, password, command):
     output = stdout.read().decode("utf-8")
     #for line in output.splitlines():
         #print(line)
-    client.close();
+    client.close()
     return output
 
 
@@ -39,28 +39,30 @@ def log_parsing(log_line):
 ###
 #test_parser= '127.0.0.1 - - [07/Dec/2021:12:26:14 +0100] "GET /index.html HTTP/1.1" 200 3477 "-" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0"'
 
-previous_date=datetime.datetime(2022, 1, 4, 12, 15, 0)
 
 
-line_parser = apache_log_parser.make_parser("%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"")
+
+
 #delay_parser = apache_log_parser.make_parser("%D")
-
-def count_difference(machine_name, fichier_log):
-    previous_date=datetime.now()-timedelta(minutes=5)
+#Fonction pour les machines 2 et 3
+def getDataM2M3(machine_name, fichier_log):
+    line_parser = apache_log_parser.make_parser("%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"")
+    previous_date=datetime.now()-timedelta(minutes=20)
     last_log=sshcmd(machine_name, 22, "interfadm", "Projet654!",("tail -1 /var/log/apache2/"+fichier_log))
     parsed_last_log=log_parsing(last_log)
     #print(parsed_last_log)
     last_line_date=parsed_last_log["time_received_datetimeobj"]
     #print(last_line_date)
     n=1
-    ip_list=[]
+    
     error_count=0
 
-    MemUsed=sshcmd("37.110.193.25", 22, "interfadm", "Projet654!","free | tail -2 | head -1 | awk '{ print $5 }' ")
+    MemUsed=sshcmd("37.110.193.25", 22, "interfadm", "Projet654!","free | grep Mem| awk '{ print $3/$2 *100.0}' ")
 
     CpuUsage=sshcmd("37.110.193.25", 22, "interfadm", "Projet654!","cat /proc/stat |grep cpu |tail -1|awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}'|awk '{print 100-$1}'")
 
     if last_line_date >= previous_date :
+        ip_list=[]
         count_page_list=[]
         diff_page_list=[]
         log_date=last_line_date
@@ -76,8 +78,9 @@ def count_difference(machine_name, fichier_log):
             if current_log_data['status']>'399' and current_log_data['status']<'600':
                 error_count+=1
 
-            if current_log_data['remote_host'] not in ip_list:
-                ip_list.append(current_log_data['remote_host'])
+            if current_log_data['remote_logname'] not in ip_list:
+                ip_list.append(current_log_data['remote_logname'])
+            #print(current_log_data['remote_logname'])
             #get visited pages and count visit number
             log_page=current_log_data['request_first_line']
 
@@ -96,9 +99,10 @@ def count_difference(machine_name, fichier_log):
             n+=1
             #Update variables
             log="tail -"+str(n)+" /var/log/apache2/"+fichier_log+" | head -1"
-            current_log=sshcmd(machine_name, 22, "interfadm", "Projet654!",log);
+            current_log=sshcmd(machine_name, 22, "interfadm", "Projet654!",log)
             current_log_data=line_parser(current_log)#Dictionnary
             log_date=current_log_data["time_received_datetimeobj"]#timestamp
+
 
         #print("Cpu Usage : ",CpuUsage)
         #print("Memory Used : ",MemUsed)
@@ -106,7 +110,86 @@ def count_difference(machine_name, fichier_log):
         #print("Error Count : ",error_count)
         #print("Unique Ips : ",len(ip_list))
         #print(n-1)
-        return[CpuUsage,MemUsed,page_list,errror_count,len(ip_list)]
+        return[CpuUsage,MemUsed,page_list,error_count,len(ip_list)]
+        
+        #pour récupérer les données : 
+        #countdifference(nom_machine, nom_fichier_logs)[0] : utilisation cpu
+        #countdifference(nom_machine, nom_fichier_logs)[1] : utilisation memoire
+        
+        #countdifference(nom_machine, nom_fichier_logs)[2][x][0/1] : page visitée et nb de fois(faire une boucle pour tout afficher)
+        #countdifference(nom_machine, nom_fichier_logs)[3] : nb erreurs
+        #countdifference(nom_machine, nom_fichier_logs)[4] : nb de connection uniques
+        
+        
+        
+        
+#Fonction pour la machine 1        
+def getDataM1(machine_name, fichier_log):
+    line_parser = apache_log_parser.make_parser("%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"")
+    previous_date=datetime.now()-timedelta(minutes=15)
+    last_log=sshcmd(machine_name, 22, "interfadm", "Projet654!",("tail -1 /var/log/apache2/"+fichier_log))
+    parsed_last_log=log_parsing(last_log)
+    #print(parsed_last_log)
+    last_line_date=parsed_last_log["time_received_datetimeobj"]
+    #print(last_line_date)
+    n=1
+    
+    error_count=0
+
+    MemUsed=sshcmd("37.110.193.25", 22, "interfadm", "Projet654!","free | grep Mem| awk '{ print $3/$2 *100.0}' ")
+
+    CpuUsage=sshcmd("37.110.193.25", 22, "interfadm", "Projet654!","cat /proc/stat |grep cpu |tail -1|awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}'|awk '{print 100-$1}'")
+
+    if last_line_date >= previous_date :
+        ip_list=[]
+        count_page_list=[]
+        diff_page_list=[]
+        log_date=last_line_date
+        page_list=[[parsed_last_log['request_first_line'],1]]
+        while log_date > previous_date:
+
+            log="tail -"+str(n)+" /var/log/apache2/"+fichier_log+" | head -1"
+            current_log=sshcmd(machine_name, 22, "interfadm", "Projet654!",log);
+            current_log_data=line_parser(current_log)#Dictionnary
+
+            #print(current_log_data["time_received_datetimeobj"])
+
+            if current_log_data['status']>'399' and current_log_data['status']<'600':
+                error_count+=1
+
+            if current_log_data['remote_host'] not in ip_list:
+                ip_list.append(current_log_data['remote_host'])
+            #print(current_log_data['remote_logname'])
+            #get visited pages and count visit number
+            log_page=current_log_data['request_first_line']
+
+            if log_page in diff_page_list:
+                count_page_list.append(log_page)
+            if log_page not in diff_page_list:
+                diff_page_list.append(log_page)
+                count_page_list.append(log_page)
+            page_list=[0]*(len(diff_page_list))
+            for i in range (0,len(diff_page_list)):
+                c=0
+                for j in range (0,len(count_page_list)):
+                    if count_page_list[j]==diff_page_list[i]:
+                        c+=1
+                page_list[i]=([diff_page_list[i],c])
+            n+=1
+            #Update variables
+            log="tail -"+str(n)+" /var/log/apache2/"+fichier_log+" | head -1"
+            current_log=sshcmd(machine_name, 22, "interfadm", "Projet654!",log)
+            current_log_data=line_parser(current_log)#Dictionnary
+            log_date=current_log_data["time_received_datetimeobj"]#timestamp
+
+
+        #print("Cpu Usage : ",CpuUsage)
+        #print("Memory Used : ",MemUsed)
+        #print(page_list)
+        #print("Error Count : ",error_count)
+        #print("Unique Ips : ",len(ip_list))
+        #print(n-1)
+        return[CpuUsage,MemUsed,page_list,error_count,len(ip_list)]
         
         #pour récupérer les données : 
         #countdifference(nom_machine, nom_fichier_logs)[0] : utilisation cpu
