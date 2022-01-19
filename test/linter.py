9f6c9512cc64634a1ca4d5f4b0f93f96e892aae9
RAHMOUNI <youssrarahmouni41@gmail.com>
2022-01-19 14:27:37 +0000

    Update linter.py

--- test/linter.py
+++ test/linter.py
@@ -5,5 +5,6 @@ import os
 import sys
 for file in sys.argv[1:]:
     print(file)
-    pylint_opts = ['--disable=trailing-whitespace', file]
+    #pylint_opts = ['--disable=trailing-whitespace', file]
+    pylint_opts = ['--rcfile=pylintrc', file]
     pylint.lint.Run(pylint_opts)
