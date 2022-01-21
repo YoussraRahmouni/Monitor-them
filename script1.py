73598951f1810871d9eded01d5b4fdf573572995
xavier <xavpalix@gmail.com>
2022-01-21 15:00:59 +0100

    getstatus + JsonFile in scritp

--- script1.py
+++ script1.py
@@ -11,40 +11,58 @@ json_string = """
     "machine1": {
         "name": "monitorme1.ddns.net",
         "log": "access.log",
+        "port": "port",
         "username":"interfadm",
         "password":"Projet654!"
-    }
+    },
     "machine2" :{
         "name": "monitorme2.ddns.net",
         "log": "other_vhosts_access.log",
+        "port": "port",
         "username":"interfadm",
         "password":"Projet654!"
-    }
+    },
     "machine3" :{
         "name": "monitorme3.ddns.net",
         "log": "other_vhosts_access.log",
+        "port": "port",
         "username":"interfadm",
         "password":"Projet654!"
     }
 }
 """
-data = json.loads(json_string)
+json_file = json.loads(json_string)
+
+def getStatus(machine_name):
+    try :
+        # test de la connexion ssh
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+        client.connect(machine_name, 22, "interfadm", "Projet654!",timeout=10)
+        client.close()
+        return 1
+    except Exception :
+        return 0
 
 def getData(machine_name, fichier_log):
 
     #instanciation de la classe log_tool
     lt=log_tool()
+    try :
+        # initialisation de la connexion ssh
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+        client.connect(machine_name, 22, "interfadm", "Projet654!",timeout=10)
+    except Exception :
+        return [0,0,[["-","-"]],"-","-",0,0]
 
-    # initialisation de la connexion ssh
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-    client.connect(machine_name, 22, "interfadm", "Projet654!")
 
     try :
         #si le log est vide on renvoie rien
         if (lt.sshcmd("wc -l /var/log/apache2/"+fichier_log+"| awk 'FNR == 1 {print $1}'",client) == "0\n"):
-            return [0,0,[["-","-"]],"-","-",0]
+            return [0,0,[["-","-"]],"-","-",0,1]
         #initialisation des données de temps (parse les dernières entrées du log
         previous_date=datetime.now()-timedelta(minutes=5)
         parsed_last_log=lt.initlastlog(fichier_log,client)
@@ -95,6 +113,8 @@ def getData(machine_name, fichier_log):
 
             #Récupération des différentes pages demandées et le nombre de requêtes correspondantes
             page_list=lt.getPageLists(count_page_list,diff_page_list)
+            if not page_list:
+                page_list[["-","-"]]
 
             #Si une ligne ou plus a été parsé, calcul de la moyenne du temps de réponse
             if n>1 :
@@ -108,5 +128,6 @@ def getData(machine_name, fichier_log):
     except Exception as e :
         client.close()
         print(e)
-        return [0,0,[["-","-"]],"Error","Error",0]
+        return [0,0,[["-","-"]],"Error","Error",0,1]
     
+
